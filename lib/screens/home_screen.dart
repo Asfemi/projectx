7fe8760a6ea004b0f7e70c6ae0943583e5fc4eaa
asfemi <pearlcstall@gmail.com>
2023-02-03 23:08:26 +0100

    baba speak to sayid on hdmi cord

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,60 +14,56 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+//todo: collect widgets from the portfollio site
+
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    List _menu = [];
-
+    
     return Scaffold(
       //appBar: PreferredSizeWidget?(),
       drawer: const Drawer(),
       body: SliverPersistentHeader(
         pinned: true,
         floating: true,
-        delegate: _SliverDelegate(
-          child: SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, int index) {
-                return Container(color: Colors.blue, height: 150.0);
-              },
-              childCount: 10,
-            ),
-          ),
-          // body: SingleChildScrollView(
-          //   child: Column(
-          //     children: [
-          //       //custom appbar
-          //       Row(children: [
-          //         const Icon(Icons.more),
-          //         Text(
-          //           'AULC',
-          //           //AULC is short for adeleke university law clinic
-          //           style: GoogleFonts.cormorantGaramond(
-          //             color: kPrimaryColor
-          //           ),
-          //           //GoogleFonts.style(),
-          //         ),
-
-          //       ]),
-          //       //landing/homepage background image
-          //       Container(
-          //        height: size.height * 0.9,
-          //        width: size.width,
-          //       decoration: const BoxDecoration(
-          //         image: DecorationImage(image: AssetImage(''))
-          //       ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-        ),
+        delegate: _SliverDelegate(child: SliverList(delegate: SliverChildBuilderDelegate((context, int index) {
+      return Container(color: Colors.blue, height: 150.0);
+    },
+    childCount: 10,),
       ),
-    );
+      // body: SingleChildScrollView(
+      //   child: Column(
+      //     children: [
+      //       //custom appbar
+      //       Row(children: [
+      //         const Icon(Icons.more),
+      //         Text(
+      //           'AULC',
+      //           //AULC is short for adeleke university law clinic
+      //           style: GoogleFonts.cormorantGaramond(
+      //             color: kPrimaryColor
+      //           ),
+      //           //GoogleFonts.style(),
+      //         ),
+
+      //       ]),
+      //       //landing/homepage background image
+      //       Container(
+      //        height: size.height * 0.9,
+      //        width: size.width,
+      //       decoration: const BoxDecoration(
+      //         image: DecorationImage(image: AssetImage(''))
+      //       ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+    ),), );
   }
 }
+
 
 class _SliverDelegate implements SliverPersistentHeaderDelegate {
   _SliverDelegate({
@@ -75,10 +71,10 @@ class _SliverDelegate implements SliverPersistentHeaderDelegate {
     this.maxHeight = 56.0,
     required this.child,
   });
-  final double minHeight;
+final double minHeight;
   final double maxHeight;
   final Widget child;
-  @override
+@override
   Widget build(
     BuildContext context,
     double shrinkOffset,
@@ -86,27 +82,24 @@ class _SliverDelegate implements SliverPersistentHeaderDelegate {
   ) {
     return SizedBox.expand(child: child);
   }
-
-  @override
+@override
   double get maxExtent => math.max(maxHeight, minHeight);
-  @override
+@override
   double get minExtent => minHeight;
-  @override
+@override
   bool shouldRebuild(_SliverDelegate oldDelegate) {
     return maxHeight != oldDelegate.maxHeight ||
         minHeight != oldDelegate.minHeight ||
         child != oldDelegate.child;
   }
-
-  @override
+@override
   Null get snapConfiguration => null;
-  @override
+@override
   Null get stretchConfiguration => null;
 
   @override
   // TODO: implement showOnScreenConfiguration
-  PersistentHeaderShowOnScreenConfiguration? get showOnScreenConfiguration =>
-      throw UnimplementedError();
+  PersistentHeaderShowOnScreenConfiguration? get showOnScreenConfiguration => throw UnimplementedError();
 
   @override
   // TODO: implement vsync
