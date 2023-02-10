a118bf5fd9018b457623b4e5647944330921c790
asfemi <pearlcstall@gmail.com>
2023-02-10 12:13:56 +0100

    ui working about

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,92 +22,30 @@ class _HomeScreenState extends State<HomeScreen> {
     Size size = MediaQuery.of(context).size;
 
     
-    return Scaffold(
-      //appBar: PreferredSizeWidget?(),
-      drawer: const Drawer(),
-      body: SliverPersistentHeader(
-        pinned: true,
-        floating: true,
-        delegate: _SliverDelegate(
-          child: SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, int index) {
-               return Container(color: Colors.blue, height: 150.0,);
-              },
-            childCount: 10,),
-          ),
-      // body: SingleChildScrollView(
-      //   child: Column(
-      //     children: [
-      //       //custom appbar
-      //       Row(children: [
-      //         const Icon(Icons.more),
-      //         Text(
-      //           'AULC',
-      //           //AULC is short for adeleke university law clinic
-      //           style: GoogleFonts.cormorantGaramond(
-      //             color: kPrimaryColor
-      //           ),
-      //           //GoogleFonts.style(),
-      //         ),
 
-      //       ]),
-      //       //landing/homepage background image
-      //       Container(
-      //        height: size.height * 0.9,
-      //        width: size.width,
-      //       decoration: const BoxDecoration(
-      //         image: DecorationImage(image: AssetImage(''))
-      //       ),
-      //       ),
-      //     ],
-      //   ),
-      // ),
-        ),
-      ), 
-    );
+
+    return CustomScrollView(
+      slivers: <Widget>[
+    const SliverAppBar(
+      pinned: true,
+      expandedHeight: 650,
+      flexibleSpace: FlexibleSpaceBar(
+        title: Text('Demo'),
+      ),
+    ),
+    SliverFixedExtentList(
+      itemExtent: 50.0,
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return Container(
+            alignment: Alignment.center,
+            color: Colors.lightBlue[100 * (index % 9)],
+            child: Text('List Item $index'),
+          );
+        },
+      ),
+    ),
+  ],
+    ); 
   }
-}
-
-
-class _SliverDelegate implements SliverPersistentHeaderDelegate {
-  _SliverDelegate({
-    this.minHeight = 40.0,
-    this.maxHeight = 56.0,
-    required this.child,
-  });
-final double minHeight;
-  final double maxHeight;
-  final Widget child;
-@override
-  Widget build(
-    BuildContext context,
-    double shrinkOffset,
-    bool overlapsContent,
-  ) {
-    return SizedBox.expand(child: child);
-  }
-@override
-  double get maxExtent => math.max(maxHeight, minHeight);
-@override
-  double get minExtent => minHeight;
-@override
-  bool shouldRebuild(_SliverDelegate oldDelegate) {
-    return maxHeight != oldDelegate.maxHeight ||
-        minHeight != oldDelegate.minHeight ||
-        child != oldDelegate.child;
-  }
-@override
-  Null get snapConfiguration => null;
-@override
-  Null get stretchConfiguration => null;
-
-  @override
-  // TODO: implement showOnScreenConfiguration
-  PersistentHeaderShowOnScreenConfiguration? get showOnScreenConfiguration => throw UnimplementedError();
-
-  @override
-  // TODO: implement vsync
-  TickerProvider? get vsync => throw UnimplementedError();
-  
 }
--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,3 +1,14 @@
+
+
 import 'package:flutter/material.dart';
 
-const Color kPrimaryColor = Colors.teal;
+//const Color kPrimaryColor = Colors.teal;
+
+class CustomColor {
+   Color teal = const Color(0xff005153);
+   Color grey1 = const Color(0xff95B0B1);
+   Color grey2 = const Color(0xff617C7C);
+  Color deepBlue = const Color(0xFF424668);
+   Color offBlue = const Color(0xFF73769B);
+  
+}
