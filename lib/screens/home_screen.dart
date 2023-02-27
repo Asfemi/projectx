ab0f053d941d7d52a1f41db7bf16f87a46d2ff7d
asfemi <pearlcstall@gmail.com>
2023-02-27 03:29:23 +0100

    created a new file for the home news widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:projectx/util/constants.dart';
 
 import '../components/custom_app_bar_widget.dart';
 import '../components/home_body_widget.dart';
+import '../components/home_news_display.dart';
 import '../components/quotes_widget.dart';
 import '../components/sections_display.dart';
 import '../components/subsribed_card.dart';
@@ -42,69 +43,7 @@ class _HomeScreenState extends State<HomeScreen> {
             QuoteWidget(size: size),
             //section display
             SectionsDisplay(prescribedHeight: prescribedHeight, size: size),
-            SizedBox(
-              //color: Colors.amber,
-              height: size.height,
-              child: ListView.builder(
-                //rather than using a listview just use a column plus a custom widget
-                //later choose to go with the existing structure
-                  itemCount: blogcontents.length,
-                  itemBuilder: (context, index) {
-                    //instead of using the if statement here
-                    //you a for statement to creat a list with four Elements
-                    //and just use it normally,
-                   // if (index != 3) {
-                      return //blog reads
-                          InkWell(
-                        onTap: () {},
-                        child: Container(
-                          padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                          height: size.height * 0.33,
-                          margin: const EdgeInsets.only(left:40, right: 40),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                flex: 1,
-                                child: Container(
-                                  child: Image(
-                                    image: AssetImage(
-                                      blogcontents[index].imagePath,
-                                    ),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 3,
-                                child: Container(
-                                  padding: const EdgeInsets.fromLTRB(40, 5, 10, 5),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        blogcontents[index].title,
-                                        style: const TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
-                                      ),
-                                      const SizedBox(
-                                        height: 30
-                                      ),
-                                      Text(
-                                        blogcontents[index].subTitle,
-                                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    // } else {
-                    //   //tbkhs
-                    // }
-                  }),
-            ),
+            HomeNewsDisplay(size: size, blogcontents: blogcontents),
             //subscribe card
             SubscribeCard(size: size),
             SizedBox(height: size.height * 0.2),
@@ -116,3 +55,4 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
