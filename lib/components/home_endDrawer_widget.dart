04fddce9c09bd437364b085023ab8571d934284a
asfemi <pearlcstall@gmail.com>
2023-03-01 18:38:42 +0100

    worked on the home screen end drawer

--- lib/components/home_endDrawer_widget.dart
+++ lib/components/home_endDrawer_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:projectx/model/directors_card_model.dart';
+import 'package:projectx/util/constants.dart';
 
 class HomeEndDrawerWidget extends StatelessWidget {
   const HomeEndDrawerWidget({
@@ -19,10 +21,83 @@ class HomeEndDrawerWidget extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.all(12.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Text('AULC LAW COPORATION', style: GoogleFonts.cormorantGaramond(
+          fontSize: 15,
+          fontWeight: FontWeight.w700,
+          color: CustomColor.teal,
+        ),),
+              EndDrawerText(text: 'OUR PEOPLE', onTap: () {}),
+              EndDrawerText(text: 'OUR EXPERTISE', onTap: () {}),
+              EndDrawerText(text: 'FOREFRONT', onTap: () {}),
+              EndDrawerText(text: 'NEWS', onTap: () {}),
+              EndDrawerText(text: 'COMUNITY', onTap: () {}),
+             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+              EndDrawerSmallerText(text: 'ABOUT US', onTap: () {}),
+              EndDrawerSmallerText(text: 'CAREERS', onTap: () {}),
+              EndDrawerSmallerText(text: 'CONTACT US', onTap: () {}),
+              EndDrawerSmallerText(text: 'CONTRIBUTION', onTap: () {}),
+              ],),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-          ],)
+class EndDrawerText extends StatelessWidget {
+  const EndDrawerText({
+    super.key,
+    required this.text,
+    required this.onTap,
+  });
+
+  final String text;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Text(
+        text,
+        style: GoogleFonts.cormorantGaramond(
+          fontSize: 30,
+          fontWeight: FontWeight.w500,
+          color: CustomColor.teal,
+        ),
+      ),
+    );
+  }
+}
+
+class EndDrawerSmallerText extends StatelessWidget {
+  const EndDrawerSmallerText({
+    super.key,
+    required this.text,
+    required this.onTap,
+  });
+
+  final String text;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Text(
+        text,
+        style: GoogleFonts.cormorantGaramond(
+          fontSize: 20,
+          fontWeight: FontWeight.w300,
+          color: CustomColor.teal,
         ),
       ),
     );
