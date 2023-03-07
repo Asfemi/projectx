722e3d8f0a5cf453eb3d497909cc757bfcdda411
asfemi <pearlcstall@gmail.com>
2023-03-07 22:48:29 +0100

    this bug no wa gree die

--- lib/components/home_drawer_widget.dart
+++ lib/components/home_drawer_widget.dart
@@ -58,14 +58,14 @@
 //                 physics: const ScrollPhysics(),
 //                 shrinkWrap: false,
 //                 itemCount: directorscontents.length,
-                
+
 //                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
 //                     crossAxisCount: 3,
 //                     childAspectRatio: MediaQuery.of(context).size.width /
 //               (MediaQuery.of(context).size.height / 3),
 //                     crossAxisSpacing: 4.0,
 //                     mainAxisSpacing: 4.0,),
-                    
+
 //                 itemBuilder: (BuildContext context, int index) {
 //                   //TODO: make this an actual network images
 //                   return Padding(
@@ -112,13 +112,14 @@
 //   }
 // }
 
-
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:projectx/model/directors_card_model.dart';
 
 class HomeDrawerWidget extends StatelessWidget {
-  const HomeDrawerWidget({Key? key, required this.size, required this.directorscontents})
+  const HomeDrawerWidget(
+      {Key? key, required this.size, required this.directorscontents})
       : super(key: key);
 
   final Size size;
@@ -126,6 +127,8 @@ class HomeDrawerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width / 2;
     return SizedBox(
       width: size.width * 0.7,
       height: size.height,
@@ -165,50 +168,47 @@ class HomeDrawerWidget extends StatelessWidget {
                 ),
               ),
               GridView.builder(
-                physics: const NeverScrollableScrollPhysics(),
+                //physics: const NeverScrollableScrollPhysics(),
                 shrinkWrap: true,
                 itemCount: directorscontents.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  childAspectRatio: 1,
-                  crossAxisSpacing: 4.0,
-                  mainAxisSpacing: 4.0,
+                //scrollDirection: Axis.vertical,
+               // controller: ScrollController(keepScrollOffset: false),
+               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCountAndFixedHeight(
+                crossAxisCount: 3,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+                height: 100.0,
                 ),
                 itemBuilder: (BuildContext context, int index) {
                   //TODO: make this an actual network images
                   return Padding(
                     padding: const EdgeInsets.all(10.0),
                     child: Column(
-                        children: [
-                          SizedBox(
-                            height: size.height * 0.40,
-                            child: Image.asset(
-                              directorscontents[index].imagePath,
-                              fit: BoxFit.cover,
-                            ),
+                      children: [
+                        Image.asset(
+                          directorscontents[index].imagePath,
+                          fit: BoxFit.cover,
+                        ),
+                        Text(
+                          directorscontents[index].title,
+                          style: GoogleFonts.cormorantGaramond(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
                           ),
-                          Text(
-                            directorscontents[index].title,
-                            style: GoogleFonts.cormorantGaramond(
-                              fontSize: 25,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black,
-                            ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          directorscontents[index].subTitle,
+                          style: GoogleFonts.montserrat(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.black,
                           ),
-                          const SizedBox(height: 10),
-                          Text(
-                            directorscontents[index].subTitle,
-                            style: GoogleFonts.montserrat(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w300,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                    
-                    
-                    
+                        ),
+                      ],
+                    ),
+
                     // SizedBox(
                     //   height: size.height * 0.4,
                     //   child: Hero(
@@ -224,6 +224,75 @@ class HomeDrawerWidget extends StatelessWidget {
               ),
             ],
           ),
-        ),),);
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class SliverGridDelegateWithFixedCrossAxisCountAndFixedHeight
+    extends SliverGridDelegate {
+  /// Creates a delegate that makes grid layouts with a fixed number of tiles in
+  /// the cross axis.
+  ///
+  /// All of the arguments must not be null. The `mainAxisSpacing` and
+  /// `crossAxisSpacing` arguments must not be negative. The `crossAxisCount`
+  /// and `childAspectRatio` arguments must be greater than zero.
+  const SliverGridDelegateWithFixedCrossAxisCountAndFixedHeight({
+    required this.crossAxisCount,
+    this.mainAxisSpacing = 0.0,
+    this.crossAxisSpacing = 0.0,
+    this.height = 56.0,
+  })  : assert(crossAxisCount != null && crossAxisCount > 0),
+        assert(mainAxisSpacing != null && mainAxisSpacing >= 0),
+        assert(crossAxisSpacing != null && crossAxisSpacing >= 0),
+        assert(height != null && height > 0);
+
+  /// The number of children in the cross axis.
+  final int crossAxisCount;
+
+  /// The number of logical pixels between each child along the main axis.
+  final double mainAxisSpacing;
+
+  /// The number of logical pixels between each child along the cross axis.
+  final double crossAxisSpacing;
+
+  /// The height of the crossAxis.
+  final double height;
+
+  bool _debugAssertIsValid() {
+    assert(crossAxisCount > 0);
+    assert(mainAxisSpacing >= 0.0);
+    assert(crossAxisSpacing >= 0.0);
+    assert(height > 0.0);
+    return true;
+  }
+
+  @override
+  SliverGridLayout getLayout(SliverConstraints constraints) {
+    assert(_debugAssertIsValid());
+    final double usableCrossAxisExtent =
+        constraints.crossAxisExtent - crossAxisSpacing * (crossAxisCount - 1);
+    final double childCrossAxisExtent = usableCrossAxisExtent / crossAxisCount;
+    final double childMainAxisExtent = height;
+    return SliverGridRegularTileLayout(
+      crossAxisCount: crossAxisCount,
+      mainAxisStride: childMainAxisExtent + mainAxisSpacing,
+      crossAxisStride: childCrossAxisExtent + crossAxisSpacing,
+      childMainAxisExtent: childMainAxisExtent,
+      childCrossAxisExtent: childCrossAxisExtent,
+      reverseCrossAxis: axisDirectionIsReversed(constraints.crossAxisDirection),
+    );
+  }
+
+  @override
+  bool shouldRelayout(
+      SliverGridDelegateWithFixedCrossAxisCountAndFixedHeight oldDelegate) {
+    return oldDelegate.crossAxisCount != crossAxisCount ||
+        oldDelegate.mainAxisSpacing != mainAxisSpacing ||
+        oldDelegate.crossAxisSpacing != crossAxisSpacing ||
+        oldDelegate.height != height;
   }
 }
